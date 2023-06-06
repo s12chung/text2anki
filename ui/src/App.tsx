7c6c89de8d98f29a222cb5c3513999244ba5abbf
Steven Chung <s12chung@users.noreply.github.com>
2023-06-05 20:02:27 -0400

    clean out to default theme

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,34 +1,8 @@
-import "./App.css"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import { useState } from "react"
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img className="logo" src={viteLogo} alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img className="logo react" src={reactLogo} alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="max-w-2xl mx-auto px-4 sm:px-6 md:px-8">
+      <div className="my-4">text2anki</div>
+    </div>
   )
 }
 
