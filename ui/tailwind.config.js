c9a6edd1d52033126b924999e8f89b56648a0225
Steven Chung <s12chung@users.noreply.github.com>
2023-06-22 19:09:43 -0400

    remove unneeded tailwind config

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -5,14 +5,11 @@ export default {
     extend: {
       fontFamily: {
         sans: 'source-han-sans-cjk-ko,"Open Sans","Gill Sans MT","Gill Sans",Corbel,Arial,sans-serif',
-        'ko-sans': 'gowun-batang,"Open Sans","Gill Sans MT","Gill Sans",Corbel,Arial,sans-serif'
+        "ko-sans": 'gowun-batang,"Open Sans","Gill Sans MT","Gill Sans",Corbel,Arial,sans-serif',
       },
       colors: {
-        'ink': 'rgba(36, 30, 32, 0.9)'
+        ink: "rgba(36, 30, 32, 0.9)",
       },
-      maxWidth: {
-        '8xl': '90rem'
-      }
     },
   },
   plugins: [],
