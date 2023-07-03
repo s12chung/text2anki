3eab516e7c5d08cd31b2ce0f19d29d2c58cfc02a
Steven Chung <s12chung@users.noreply.github.com>
2023-07-03 17:24:46 -0400

    clean make test.fixtures output

--- Makefile_env.mk
+++ Makefile_env.mk
@@ -1,3 +1,5 @@
 TAGS ?= sqlite_math_functions
 CI_BIN ?= .github/bin
 PATH := $(PWD)/$(CI_BIN):$(PATH)
+
+FIXTURE_CLEAN_OUTPUT = awk 'BEGIN{ORS=""; RS=""} {gsub(/\n[^\n]*    fixture\.go([^\n]*\n){2,6}[^\n]*turn off ENV var to run test/, " - UPDATE_FIXTURES=true")}1'
