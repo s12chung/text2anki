e1c70c4e289dc949ea967ba8c9eacf137b78b9df
Steven Chung <s12chung@users.noreply.github.com>
2023-08-01 20:42:11 -0400

    components/Header.tsx

--- ui/src/pages/layouts/FullLayout.tsx
+++ ui/src/pages/layouts/FullLayout.tsx
@@ -1,11 +1,10 @@
-import { Link, Outlet } from "react-router-dom"
+import Header from "../../components/Header.tsx"
+import { Outlet } from "react-router-dom"
 
 const FullLayout: React.FC = () => {
   return (
     <div className="m-std">
-      <div className="py-std">
-        <Link to="/">text2anki</Link>
-      </div>
+      <Header />
       <div>
         <Outlet />
       </div>
