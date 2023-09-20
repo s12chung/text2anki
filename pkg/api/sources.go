5003a9aa15b2bde453f8569f1d45236e151d9cd2
Steven Chung <s12chung@users.noreply.github.com>
2023-09-20 19:59:15 -0400

    refactor Routes.SourceCreate

--- pkg/api/sources.go
+++ pkg/api/sources.go
@@ -56,19 +56,7 @@ func (rs Routes) SourceCreate(r *http.Request, txQs db.TxQs) (any, *jhttp.HTTPEr
 	if httpErr := extractAndValidate(r, &req); httpErr != nil {
 		return nil, httpErr
 	}
-
-	var prePartList *db.PrePartList
-	if req.PrePartListID != "" {
-		prePartList = &db.PrePartList{}
-		if err := rs.Storage.DBStorage.KeyTree(db.SourcesTable, db.PartsColumn, req.PrePartListID, prePartList); err != nil {
-			if storage.IsNotFoundError(err) {
-				return nil, jhttp.Error(http.StatusNotFound, err)
-			}
-			return nil, jhttp.Error(http.StatusInternalServerError, err)
-		}
-	}
-
-	source, err := rs.sourceCreateSource(r.Context(), req, prePartList)
+	source, err := rs.sourceStructuredFromBodyReq(r.Context(), req)
 	if err != nil {
 		return nil, err
 	}
@@ -121,6 +109,20 @@ func (rs Routes) SourceDestroy(r *http.Request, txQs db.TxQs) (any, *jhttp.HTTPE
 	})
 }
 
+func (rs Routes) prePartListFromID(prePartListID string) (*db.PrePartList, *jhttp.HTTPError) {
+	if prePartListID == "" {
+		return nil, nil
+	}
+	prePartList := &db.PrePartList{}
+	if err := rs.Storage.DBStorage.KeyTree(db.SourcesTable, db.PartsColumn, prePartListID, prePartList); err != nil {
+		if storage.IsNotFoundError(err) {
+			return nil, jhttp.Error(http.StatusNotFound, err)
+		}
+		return nil, jhttp.Error(http.StatusInternalServerError, err)
+	}
+	return prePartList, nil
+}
+
 func sourceStructuredFromID(r *http.Request, txQs db.TxQs) (db.SourceStructured, *jhttp.HTTPError) {
 	id, httpErr := idFromRequest(r)
 	if httpErr != nil {
@@ -133,9 +135,13 @@ func sourceStructuredFromID(r *http.Request, txQs db.TxQs) (db.SourceStructured,
 	return source.ToSourceStructured(), nil
 }
 
-func (rs Routes) sourceCreateSource(ctx context.Context, req SourceCreateRequest,
-	prePartList *db.PrePartList) (*db.SourceStructured, *jhttp.HTTPError) {
-	name, reference, err := rs.sourceCreateSourceNameRef(req.Name, req.Reference, prePartList)
+func (rs Routes) sourceStructuredFromBodyReq(ctx context.Context, req SourceCreateRequest) (*db.SourceStructured, *jhttp.HTTPError) {
+	prePartList, httpErr := rs.prePartListFromID(req.PrePartListID)
+	if httpErr != nil {
+		return nil, httpErr
+	}
+
+	name, reference, err := rs.chooseSourceNameRef(req.Name, req.Reference, prePartList)
 	if err != nil {
 		return nil, err
 	}
@@ -149,20 +155,19 @@ func (rs Routes) sourceCreateSource(ctx context.Context, req SourceCreateRequest
 		if err != nil {
 			return nil, jhttp.Error(http.StatusUnprocessableEntity, err)
 		}
-		part := db.SourcePart{TokenizedTexts: tokenizedTexts}
+		sourcePart := db.SourcePart{TokenizedTexts: tokenizedTexts}
 		if prePartList != nil {
-			part.Media = &prePartList.PreParts[i]
+			sourcePart.Media = &prePartList.PreParts[i]
 		}
-		parts = append(parts, part)
+		parts = append(parts, sourcePart)
 	}
-
 	if len(parts) == 0 {
 		return nil, jhttp.Error(http.StatusUnprocessableEntity, fmt.Errorf("no parts found with text set"))
 	}
 	return &db.SourceStructured{Name: name, Reference: reference, Parts: parts}, nil
 }
 
-func (rs Routes) sourceCreateSourceNameRef(name, ref string, prePartList *db.PrePartList) (string, string, *jhttp.HTTPError) {
+func (rs Routes) chooseSourceNameRef(name, ref string, prePartList *db.PrePartList) (string, string, *jhttp.HTTPError) {
 	if prePartList == nil || (name != "" && ref != "") {
 		return name, ref, nil
 	}
