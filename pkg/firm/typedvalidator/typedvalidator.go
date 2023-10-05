6a73c9ae38ce862abbad9f5ae5a5a864f33fc4af
Steven Chung <s12chung@users.noreply.github.com>
2023-10-05 13:12:58 -0400

    typedvalidator

---
+++ pkg/firm/typedvalidator/typedvalidator.go
@@ -0,0 +1,89 @@
+// Package typedvalidator contained typed validators with generics
+package typedvalidator
+
+import (
+	"reflect"
+
+	"github.com/s12chung/text2anki/pkg/firm"
+)
+
+// TypedValidator is a generic firm.Validator that has a typed ValidateTyped() function
+type TypedValidator[T any] interface {
+	firm.Validator
+	ValidateTyped(data T) firm.ErrorMap
+}
+
+// MustNewStruct returns a new Struct, panics if there is an error
+func MustNewStruct[T any](data T, ruleMap firm.RuleMap) Struct[T] {
+	return mustNew(func() (Struct[T], error) { return NewStruct(data, ruleMap) })
+}
+
+// NewStruct returns a new Struct
+func NewStruct[T any](data T, ruleMap firm.RuleMap) (Struct[T], error) {
+	validator, err := firm.NewStructValidator(data, ruleMap)
+	if err != nil {
+		return Struct[T]{}, err
+	}
+	return Struct[T]{StructValidator: validator}, nil
+}
+
+// Struct is a StructValidator that's typed
+type Struct[T any] struct{ firm.StructValidator }
+
+// ValidateTyped is firm.Validator.WillV(), but with a typed arg, so no type checking is done on runtime
+func (s Struct[T]) ValidateTyped(data T) firm.ErrorMap { return validateTyped(s, data) }
+
+// MustNewSlice returns a new Slice, panics if there is an error
+func MustNewSlice[T any](data T, elementRules ...firm.Rule) Slice[T] {
+	return mustNew(func() (Slice[T], error) { return NewSlice[T](data, elementRules...) })
+}
+
+// NewSlice returns a new Slice
+func NewSlice[T any](data T, elementRules ...firm.Rule) (Slice[T], error) {
+	validator, err := firm.NewSliceValidator(data, elementRules...)
+	if err != nil {
+		return Slice[T]{}, err
+	}
+	return Slice[T]{SliceValidator: validator}, nil
+}
+
+// Slice is a SliceValidator that's typed
+type Slice[T any] struct{ firm.SliceValidator }
+
+// ValidateTyped is firm.Validator.WillV(), but with a typed arg, so no type checking is done on runtime
+func (s Slice[T]) ValidateTyped(data T) firm.ErrorMap { return validateTyped(s, data) }
+
+// MustNewValue returns a new Value, panics if there is an error
+func MustNewValue[T any](data T, rules ...firm.Rule) Value[T] {
+	return mustNew(func() (Value[T], error) { return NewValue[T](data, rules...) })
+}
+
+// NewValue returns a new Value
+func NewValue[T any](data T, rules ...firm.Rule) (Value[T], error) {
+	validator, err := firm.NewValueValidator(data, rules...)
+	if err != nil {
+		return Value[T]{}, err
+	}
+	return Value[T]{ValueValidator: validator}, nil
+}
+
+// Value is a ValueValidator that's typed
+type Value[T any] struct{ firm.ValueValidator }
+
+// ValidateTyped is firm.Validator.WillV(), but with a typed arg, so no type checking is done on runtime
+func (v Value[T]) ValidateTyped(data T) firm.ErrorMap { return validateTyped(v, data) }
+
+func mustNew[T any](f func() (T, error)) T {
+	validator, err := f()
+	if err != nil {
+		panic(err.Error())
+	}
+	return validator
+}
+
+func validateTyped(validator firm.Validator, data any) firm.ErrorMap {
+	value := reflect.ValueOf(data)
+	errorMap := firm.ErrorMap{}
+	validator.ValidateMerge(value, firm.TypeNameKey(value), errorMap)
+	return errorMap.ToNil()
+}
---
+++ pkg/firm/typedvalidator/typedvalidator_test.go
@@ -0,0 +1,185 @@
+package typedvalidator
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/s12chung/text2anki/pkg/firm"
+)
+
+type presentRule struct{}
+
+const presentRuleKey = "presentRule"
+
+var errTest = &firm.TemplatedError{Template: "test"}
+
+func (p presentRule) ValidateValue(value reflect.Value) firm.ErrorMap {
+	if !value.IsValid() || value.IsZero() {
+		return firm.ErrorMap{presentRuleKey: errTest}
+	}
+	return nil
+}
+func (p presentRule) ValidateType(_ reflect.Type) *firm.RuleTypeError { return nil }
+
+type onlyKindRule struct{ kind reflect.Kind }
+
+func (o onlyKindRule) ValidateValue(_ reflect.Value) firm.ErrorMap { return nil }
+func (o onlyKindRule) ValidateType(typ reflect.Type) *firm.RuleTypeError {
+	if typ.Kind() != o.kind {
+		return firm.NewRuleTypeError(typ, "is not "+o.kind.String())
+	}
+	return nil
+}
+
+type testStruct struct {
+	WillV string
+	NoV   string
+}
+
+func TestNewStruct(t *testing.T) {
+	tcs := []struct {
+		name    string
+		ruleMap firm.RuleMap
+		err     error
+	}{
+		{name: "normal", ruleMap: firm.RuleMap{"WillV": []firm.Rule{presentRule{}}}},
+		{name: "bad_rule", ruleMap: firm.RuleMap{"NotAField": []firm.Rule{presentRule{}}},
+			err: fmt.Errorf("field, NotAField, not found in type: typedvalidator.testStruct")},
+	}
+	for _, tc := range tcs {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			require := require.New(t)
+
+			validator, err := NewStruct(testStruct{}, tc.ruleMap)
+			if tc.err != nil {
+				require.Equal(tc.err, err)
+				return
+			}
+
+			require.NoError(err)
+			expected, err := firm.NewStructValidator(testStruct{}, tc.ruleMap)
+			require.NoError(err)
+
+			require.Equal(expected, validator.StructValidator)
+		})
+	}
+}
+
+func TestStruct_ValidateTyped(t *testing.T) {
+	tcs := []validateTypedTC[testStruct]{
+		{name: "valid", data: testStruct{WillV: "ok"}},
+		{name: "invalid", data: testStruct{NoV: "not_ok"},
+			result: firm.ErrorMap{"typedvalidator.testStruct.WillV." + presentRuleKey: errTest}},
+	}
+	testValidateTyped(t, tcs, func() (TypedValidator[testStruct], error) {
+		return NewStruct(testStruct{}, firm.RuleMap{"WillV": []firm.Rule{presentRule{}}})
+	})
+}
+
+func TestNewSlice(t *testing.T) {
+	intKindRule := onlyKindRule{kind: reflect.Int}
+
+	tcs := []struct {
+		name         string
+		elementRules []firm.Rule
+		err          error
+	}{
+		{name: "normal", elementRules: []firm.Rule{presentRule{}}},
+		{name: "bad_rule", elementRules: []firm.Rule{intKindRule},
+			err: fmt.Errorf("element type: %w", intKindRule.ValidateType(reflect.TypeOf(testStruct{})))},
+	}
+	for _, tc := range tcs {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			require := require.New(t)
+
+			validator, err := NewSlice([]testStruct{}, tc.elementRules...)
+			if tc.err != nil {
+				require.Equal(tc.err, err)
+				return
+			}
+
+			require.NoError(err)
+			expected, err := firm.NewSliceValidator([]testStruct{}, tc.elementRules...)
+			require.NoError(err)
+
+			require.Equal(expected, validator.SliceValidator)
+		})
+	}
+}
+
+func TestSlice_ValidateTyped(t *testing.T) {
+	tcs := []validateTypedTC[[]testStruct]{
+		{name: "valid", data: []testStruct{{WillV: "ok"}}},
+		{name: "invalid", data: []testStruct{{}},
+			result: firm.ErrorMap{"[]typedvalidator.testStruct.[0]." + presentRuleKey: errTest}},
+	}
+	testValidateTyped(t, tcs, func() (TypedValidator[[]testStruct], error) {
+		return NewSlice([]testStruct{}, presentRule{})
+	})
+}
+
+func TestMustNewValue(t *testing.T) {
+	intKindRule := onlyKindRule{kind: reflect.Int}
+
+	tcs := []struct {
+		name  string
+		rules []firm.Rule
+		err   error
+	}{
+		{name: "normal", rules: []firm.Rule{presentRule{}}},
+		{name: "bad_rule", rules: []firm.Rule{intKindRule}, err: intKindRule.ValidateType(reflect.TypeOf(testStruct{}))},
+	}
+	for _, tc := range tcs {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			require := require.New(t)
+
+			validator, err := NewValue(testStruct{}, tc.rules...)
+			if tc.err != nil {
+				require.Equal(tc.err, err)
+				return
+			}
+
+			require.NoError(err)
+			expected, err := firm.NewValueValidator(testStruct{}, tc.rules...)
+			require.NoError(err)
+
+			require.Equal(expected, validator.ValueValidator)
+		})
+	}
+}
+
+func TestValue_ValidateTyped(t *testing.T) {
+	tcs := []validateTypedTC[testStruct]{
+		{name: "valid", data: testStruct{WillV: "ok"}},
+		{name: "invalid", data: testStruct{},
+			result: firm.ErrorMap{"typedvalidator.testStruct." + presentRuleKey: errTest}},
+	}
+	testValidateTyped(t, tcs, func() (TypedValidator[testStruct], error) {
+		return NewValue(testStruct{}, presentRule{})
+	})
+}
+
+type validateTypedTC[T any] struct {
+	name   string
+	data   T
+	result firm.ErrorMap
+}
+
+func testValidateTyped[T any](t *testing.T, tcs []validateTypedTC[T], newValidator func() (TypedValidator[T], error)) {
+	validator, err := newValidator()
+	require.NoError(t, err)
+
+	for _, tc := range tcs {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			require.Equal(t, tc.result, validator.ValidateTyped(tc.data))
+			require.Equal(t, tc.result, validator.Validate(tc.data))
+		})
+	}
+}
