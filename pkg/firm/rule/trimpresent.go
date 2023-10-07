031eceed028d655b6e07dd5067e911edd9b9ff71
Steven Chung <s12chung@users.noreply.github.com>
2023-10-07 19:06:50 -0400

    revert back TrimPresent implementation for speed

--- pkg/firm/rule/trimpresent.go
+++ pkg/firm/rule/trimpresent.go
@@ -2,19 +2,17 @@ package rule
 
 import (
 	"reflect"
+	"strings"
 
 	"github.com/s12chung/text2anki/pkg/firm"
-	"github.com/s12chung/text2anki/pkg/firm/attr"
 )
 
 // TrimPresent checks if data is not "" when strings.TrimSpace is applied
 type TrimPresent struct{}
 
-var trimPresentAlias = Attr{Of: attr.TrimSpace{}, Rule: Present{}}
-
 // ValidateValue returns true if the data is valid (assumes ValidateType is called)
 func (t TrimPresent) ValidateValue(value reflect.Value) firm.ErrorMap {
-	if err := trimPresentAlias.ValidateValue(value); err != nil {
+	if strings.TrimSpace(value.String()) == "" {
 		return errorMapTrimPresent
 	}
 	return nil
@@ -22,7 +20,10 @@ func (t TrimPresent) ValidateValue(value reflect.Value) firm.ErrorMap {
 
 // ValidateType checks whether the type is valid for the Rule
 func (t TrimPresent) ValidateType(typ reflect.Type) *firm.RuleTypeError {
-	return trimPresentAlias.ValidateType(typ)
+	if typ.Kind() != reflect.String {
+		return firm.NewRuleTypeError(typ, "is not a String")
+	}
+	return nil
 }
 
 var errorMapTrimPresent = firm.ErrorMap{"TrimPresent": firm.TemplateError{Template: "is just spaces or empty"}}
